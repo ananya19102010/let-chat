49b052e90c6d2e18bcd4db801d3ea57c97331669
ananya19102010 <73178449+ananya19102010@users.noreply.github.com>
2021-06-07 19:58:08 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBPREXb7Yac8HYeuTS4Uk88hHKsmSJKzvg",
+    authDomain: "project-44-a7e64.firebaseapp.com",
+    databaseURL: "https://project-44-a7e64-default-rtdb.firebaseio.com",
+    projectId: "project-44-a7e64",
+    storageBucket: "project-44-a7e64.appspot.com",
+    messagingSenderId: "863119522087",
+    appId: "1:863119522087:web:a542cdfc6ad8c9981d5102"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
